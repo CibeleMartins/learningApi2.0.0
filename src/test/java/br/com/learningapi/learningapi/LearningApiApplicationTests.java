5aef08d8e9ca9048d72b23c44928ae71d0b87840
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-05 19:41:14 -0300

    ♻️ feat: refatorou tudo

---
+++ src/test/java/br/com/learningapi/learningapi/LearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learningapi.learningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
