0041d05244dde951936f605802aec38b24e9e751
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-06 10:11:18 -0300

    🚧 feat: add getters e setters, definiu colunas e relacionamentos no model de anotações

--- src/main/java/br/com/learningapi/learningapi/domain/model/UserAnnotation.java
+++ src/main/java/br/com/learningapi/learningapi/domain/model/UserAnnotation.java
@@ -2,9 +2,42 @@ package br.com.learningapi.learningapi.domain.model;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "annotations")
 public class UserAnnotation {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_annotation")
     private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Date date;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String annotation;
+
+    @Column(nullable = true)
+    private String color;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UserLearner user;
 
     public Long getId() {
         return id;
@@ -54,13 +87,5 @@ public class UserAnnotation {
         this.color = color;
     }
 
-    private String title;
-
-    private Date date;
-
-    private String description;
-
-    private String annotation;
-
-    private String color;
+  
 }
