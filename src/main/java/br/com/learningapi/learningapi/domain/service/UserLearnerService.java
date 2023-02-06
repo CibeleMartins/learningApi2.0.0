c048e36aaec8afea850900225f7d0d0d2b4baaf3
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-06 10:32:28 -0300

    🚧 feat: add serviço de usuário

--- src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
+++ src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
@@ -1,5 +1,39 @@
 package br.com.learningapi.learningapi.domain.service;
 
-public class UserLearnerService {
-    
+import java.util.List;
+
+import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerRequest;
+import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerResponse;
+
+public class UserLearnerService implements CrudService<UserLearnerRequest, UserLearnerResponse> {
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public List<UserLearnerResponse> getAll() {
+
+        return null;
+    }
+
+    @Override
+    public UserLearnerResponse getById(Long id) {
+
+        return null;
+    }
+
+    @Override
+    public UserLearnerResponse register(UserLearnerRequest dto) {
+
+        return null;
+    }
+
+    @Override
+    public UserLearnerResponse updateById(Long id, UserLearnerRequest dto) {
+
+        return null;
+    }
+
 }
