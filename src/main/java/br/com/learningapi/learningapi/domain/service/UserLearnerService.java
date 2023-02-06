ace32ee0a6e6ec6d95dfc1321de5a72f74a66cf8
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-06 10:46:22 -0300

    🚧 feat: add método obter todos e obter pelo id do usuário

--- src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
+++ src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
@@ -1,10 +1,13 @@
 package br.com.learningapi.learningapi.domain.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import br.com.learningapi.learningapi.domain.model.UserLearner;
 import br.com.learningapi.learningapi.domain.repository.UserLearnerRepository;
 import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerRequest;
 import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerResponse;
@@ -25,13 +28,25 @@ public class UserLearnerService implements CrudService<UserLearnerRequest, UserL
     @Override
     public List<UserLearnerResponse> getAll() {
 
-        return null;
+        List<UserLearner> usersLearnersModel = userLearnerRepository.findAll();
+
+        List<UserLearnerResponse> usersLearnersResponse = usersLearnersModel.stream().map(u -> mapper.map(u, UserLearnerResponse.class)).collect(Collectors.toList());
+        
+        return usersLearnersResponse;
     }
 
     @Override
     public UserLearnerResponse getById(Long id) {
 
-        return null;
+        Optional<UserLearner> userLearnerModel = userLearnerRepository.findById(id);
+
+        if(userLearnerModel.isEmpty()) {
+            throw new Error("O usuário de id: " + id + " não ofi encontrado.");
+        }
+
+        UserLearnerResponse userLearnerResponse = mapper.map(userLearnerModel.get(), UserLearnerResponse.class);
+
+        return userLearnerResponse;
     }
 
     @Override
