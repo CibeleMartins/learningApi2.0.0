f836b37ff740050124fc5f68376b70be51e91406
CibeleMartins <cibelemartins24@hotmail.com>
2023-02-06 10:38:58 -0300

    🚧 feat: add config do model mapper e implementou o serviço de usuário

---
+++ src/main/java/br/com/learningapi/learningapi/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package br.com.learningapi.learningapi.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+}
--- src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
+++ src/main/java/br/com/learningapi/learningapi/domain/service/UserLearnerService.java
@@ -2,10 +2,20 @@ package br.com.learningapi.learningapi.domain.service;
 
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.learningapi.learningapi.domain.repository.UserLearnerRepository;
 import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerRequest;
 import br.com.learningapi.learningapi.dto.UserAnnotation.UserLearnerResponse;
 
 public class UserLearnerService implements CrudService<UserLearnerRequest, UserLearnerResponse> {
+
+    @Autowired
+    private UserLearnerRepository userLearnerRepository;
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public void deleteById(Long id) {
